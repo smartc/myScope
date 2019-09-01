fef50632a60cd659a6091a02c6e77c181358b788
Corey Smart <corey.smart@gmail.com>
2019-08-31 20:03:54 -0600

    Sync working from command line. TODO: Selection of GPS port TODO: Store config data in json file

--- gps_test.py
+++ gps_test.py
@@ -1,39 +1,8 @@
 from micropyGPS import MicropyGPS
 from serial import Serial
 import win32com.client
-
-GPS_PORT = 'COM4'
-gps = Serial(GPS_PORT)
-
-myGPS = MicropyGPS()
-go = True
-
-while go==True:
-	gps_data = gps.readline().decode("utf-8").rstrip()
-	print("RAW GPS Data:  {}".format(gps_data))
-	for x in gps_data:
-		myGPS.update(x)
-
-	print("Latitude  = {}".format(myGPS.latitude))
-	print("Longitude = {}".format(myGPS.longitude))
-	print("Altitude  = {}".format(myGPS.altitude))
-	print("UTC Time  = {}".format(myGPS.timestamp))
-	print("UTC Date  = {}".format(myGPS.date))
-	print("\n")
-
-	if myGPS.valid == True and myGPS.latitude != [0, 0.0, 'N']:
-			go = False
-
-gps_lat = myGPS.latitude[0] + myGPS.latitude[1]/60
-if myGPS.latitude[2] == "S":
-	gps_lat = -gps_lat
-
-gps_lon = myGPS.longitude[0] + myGPS.longitude[1]/60
-if myGPS.longitude[2] == "W":
-	gps_lon = -gps_lon
-
-print("Latitude DD.DDDD  = {:.4f}".format(gps_lat))
-print("Longitude DD.DDDD = {:.4f}".format(gps_lon))
+from pytz import UTC
+from datetime import datetime
 
 
 x = win32com.client.Dispatch("ASCOM.Utilities.Chooser")
@@ -42,11 +11,42 @@ device = x.Choose(None)
 tel = win32com.client.Dispatch(device)
 tel.Connected = True
 
-print(tel.SiteLatitude)
-print(tel.SiteLongitude)
+# Establish GPS
+GPS_PORT = 'COM4'
+gps = Serial(GPS_PORT)
+
+myGPS = MicropyGPS()
+
+# Retrieve GPS position data
+while myGPS.valid == False or myGPS.latitude == [0, 0.0, 'N']:
+	gps_data = gps.readline().decode("utf-8").rstrip()
+	for x in gps_data:
+		myGPS.update(x)
+
+# Set Telescope Date/Time
+d = UTC.localize(datetime(myGPS.date[0], myGPS.date[1], myGPS.date[2],\
+ myGPS.timestamp[0], myGPS.timestamp[1], int(myGPS.timestamp[2])))
+tel.UTCDate = d
+
+# Set Telescope Position, Altitude
+gps_lat = myGPS.latitude[0] + myGPS.latitude[1]/60
+if myGPS.latitude[2] == "S":
+	gps_lat = -gps_lat
+
+gps_lon = myGPS.longitude[0] + myGPS.longitude[1]/60
+if myGPS.longitude[2] == "W":
+	gps_lon = -gps_lon
 
 tel.SiteLatitude = gps_lat
 tel.SiteLongitude = gps_lon
+tel.SiteElevation = myGPS.altitude
 
-print(tel.SiteLatitude)
-print(tel.SiteLongitude)
+print("+-------------------------------------------------+")
+print("| Site Latitude          |  {:<+9.4f}             |".format(tel.SiteLatitude))
+print("| Site Longitude         |  {:<+9.4f}             |".format(tel.SiteLongitude))
+print("| Site Elevation         |  {:<+7.2f}               |".format(tel.SiteElevation))
+print("| Scope Date             |  {:%Y/%m/%d}            |".format(tel.UTCDate))
+print("| Scope Time             |  {:%H:%M:%S}              |".format(tel.UTCDate))
+print("+-------------------------------------------------+")
+
+tel.Connected = False
