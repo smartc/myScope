c946abc5b61f96e01523da6e1d21b178b82a74ac
Corey Smart <corey.smart@gmail.com>
2019-09-08 09:14:39 -0600

    Updated threading syntax as Worker class

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from datetime import datetime
 
 class Clock(tk.Label):
-    def __init__(self, parent, clock_time = None, suffix=None):
+    def __init__(self, parent, clock_time = None, suffix=None, format='%H:%M:%S'):
         tk.Label.__init__(self, parent)
         self.stop = False
         if suffix is not None:
@@ -12,21 +12,21 @@ class Clock(tk.Label):
         if clock_time is None:
             self.t1 = None
             self.t2 = None
-            self.time = datetime.now().strftime('%H:%M:%S') + self.suffix
+            self.time = datetime.now().strftime(format) + self.suffix
         else:
             self.t0 = clock_time
             self.t1 = datetime.now()
             self.t2 = None
-            self.time = clock_time.strftime('%H:%M:%S') + self.suffix
+            self.time = clock_time.strftime(format) + self.suffix
 
         self.configure(text=self.time)
 
         
-    def tick(self):
+    def tick(self, format='%H:%M:%S'):
         if self.t1 is None:
-            self.time = datetime.now().strftime('%H:%M:%S') + self.suffix
+            self.time = datetime.now().strftime(format) + self.suffix
         else:
             self.t2 = datetime.now()
-            self.time = (self.t0 + (self.t2 - self.t1)).strftime('%H:%M:%S') + self.suffix
+            self.time = (self.t0 + (self.t2 - self.t1)).strftime(format) + self.suffix
 
         self.configure(text=self.time)
--- clock_test.py
+++ clock_test.py
@@ -1,18 +1,61 @@
 from clock import Clock
 from datetime import datetime
 import tkinter as tk
-from threading import Thread
+import threading
+import ctypes
 from time import sleep
 
 def exit_event():
 	global app
-	app.stop = True
+	global watcherThread
+	watcherThread.raise_exception()
+	sleep(0.25)
 	app.destroy()
+	watcherThread.join()
 
-def watcher(root):
-    while not root.stop:
-        root.data.scope_time.tick()
-        sleep(200/1000)
+class Worker(threading.Thread):
+	def __init__(self, name):
+		threading.Thread.__init__(self)
+		self.name = name
+
+	def run(self):
+		global app
+		try:
+			while not app.stop:
+				app.data.scope_time.tick()
+				app.data.scope_date.tick('%Y-%m-%d')
+				app.data.gps_time.tick()
+				app.data.gps_date.tick('%Y-%m-%d')
+				sleep(0.2)
+		finally:
+			print("terminating {}".format(self.name))
+
+	def get_id(self):
+		if hasattr(self, '_thread_id'):
+			return self._thread_id
+		for id, thread in threading._active.items():
+			if thread is self:
+				return id
+
+	def raise_exception(self):
+		global app
+		app.stop = True
+		thread_id = self.get_id()
+		res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 
+			ctypes.py_object(SystemExit))
+		if res > 1: 
+			ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0) 
+			print('Exception raise failure')
+		
+def watcher():
+	global app
+	while not app.stop:
+		app.data.scope_time.tick()
+		app.data.scope_date.tick('%Y-%m-%d')
+		app.data.gps_time.tick()
+		app.data.gps_date.tick('%Y-%m-%d')
+		print(app.data.gps_time.time)
+		sleep(200/1000)
 
 class DataFrame(tk.Frame):
 	def __init__(self, parent):
@@ -28,8 +71,18 @@ class DataFrame(tk.Frame):
 		SCP_LAT = 50.5555
 		SCP_LON = -114.3014
 		SCP_ALT = 1250.0
-		SCP_DATE = datetime(2018, 9, 1, 15, 33, 23)
+		SCP_DATE = datetime(2018, 9, 1, 23, 59, 23)
 		#######################################
+
+		#######################################
+		#testing variables
+		GPS_LAT = 50.699420
+		GPS_LON = -114.005746
+		GPS_ALT = 1107.0
+		GPS_DATE = datetime(2018, 9, 1, 23, 59, 23)
+		#######################################
+
+
 
 		self.topFrame = tk.Frame(self, width=WIDTH-10, height=50, pady=PAD)
 		self.centerFrame = tk.Frame(self, bg='gray', width=WIDTH-10, height=HEIGHT-120, padx=PAD, pady=PAD)
@@ -87,9 +140,10 @@ class DataFrame(tk.Frame):
 		self.scope_alt = tk.Label(self.left_panel, text="{:<+,.1f} m".format(int(SCP_ALT)), font=FONT, relief="groove", padx=3, pady=3)
 
 		self.scope_time = Clock(self.left_panel, SCP_DATE, " UTC")
-		self.scope_time.configure(font=FONT, relief="groove", padx=3, pady=3)
+		self.scope_time.configure(font=FONT, fg='blue', relief="groove", padx=3, pady=3)
 	
-		self.scope_date = tk.Label(self.left_panel, text="{:%Y-%m-%d}".format(SCP_DATE), font=FONT, relief="groove", padx=3, pady=3)
+		self.scope_date = Clock(self.left_panel, SCP_DATE, None, '%Y-%m-%d')
+		self.scope_date.configure(font=FONT, fg='blue', relief="groove", padx=3, pady=3)
 
 		self.scope_lat.grid(row=0, column=1, sticky="ew")
 		self.scope_lon.grid(row=1, column=1, sticky="ew")
@@ -98,16 +152,50 @@ class DataFrame(tk.Frame):
 		self.scope_date.grid(row=4, column=1, sticky="ew")
 
 
+		#setup GPS frame
+		lat2 = tk.Label(self.right_panel, text="LAT:", anchor="e", width=10, font=FONT, padx=3, pady=3)
+		lon2 = tk.Label(self.right_panel, text="LON:", anchor="e", width=10, font=FONT, padx=3, pady=3)
+		alt2 = tk.Label(self.right_panel, text="ALT:", anchor="e", width=10, font=FONT, padx=3, pady=3)
+		time2 = tk.Label(self.right_panel, text="TIME:", anchor="e", width=10, font=FONT, padx=3, pady=3)
+		date2 = tk.Label(self.right_panel, text="DATE:", anchor="e", width=10, font=FONT, padx=3, pady=3)
+
+		lat2.grid(row=0, column=0, sticky="ew")
+		lon2.grid(row=1, column=0, sticky="ew")
+		alt2.grid(row=2, column=0, sticky="ew")
+		time2.grid(row=3, column=0, sticky="ew")
+		date2.grid(row=4, column=0, sticky="ew")
+
+		#setup GPS data panel
+
+		self.gps_lat = tk.Label(self.right_panel, text="{:<+.4f} °".format(GPS_LAT), font=FONT, relief="groove", padx=3, pady=3)
+		self.gps_lon = tk.Label(self.right_panel, text="{:<+.4f} °".format(GPS_LON), font=FONT, relief="groove", padx=3, pady=3)
+		self.gps_alt = tk.Label(self.right_panel, text="{:<+,.1f} m".format(int(GPS_ALT)), font=FONT, relief="groove", padx=3, pady=3)
+
+		self.gps_time = Clock(self.right_panel, GPS_DATE, " UTC")
+		self.gps_time.configure(font=FONT, fg='blue', relief="groove", padx=3, pady=3)
+	
+		self.gps_date = Clock(self.right_panel, GPS_DATE, None, '%Y-%m-%d')
+		self.gps_date.configure(font=FONT, fg='blue', relief="groove", padx=3, pady=3)
+
+		self.gps_lat.grid(row=0, column=1, sticky="ew")
+		self.gps_lon.grid(row=1, column=1, sticky="ew")
+		self.gps_alt.grid(row=2, column=1, sticky="ew")
+		self.gps_time.grid(row=3, column=1, sticky="ew")
+		self.gps_date.grid(row=4, column=1, sticky="ew")
+
 
 
 if __name__ == "__main__":
 	global app
+	global watcherThread
 	app = tk.Tk()
 	app.protocol("WM_DELETE_WINDOW", exit_event)
 	app.stop = False
 	app.data = DataFrame(app)
 	app.data.pack()
 
-	watcherThread = Thread(target=watcher, kwargs={'root': app}, name="tick_tock")
+#	watcherThread = threading.Thread(target=watcher, name="tick_tock")
+#	watcherThread.start()
+	watcherThread = Worker("clock_watcher")
 	watcherThread.start()
 	app.mainloop()
